a806438af9d16593febf00e58cded3d7c4e7b80a
guilima2304 <gui.lima2304@gmail.com>
2020-12-21 21:30:26 -0300

    alinhamento de inputs

--- src/sections/contact-us/index.js
+++ src/sections/contact-us/index.js
@@ -14,36 +14,21 @@ export default class ContactUs extends React.Component {
                     <div className="section-content">
                         <div className="content-contact-us">
                             <h2>Fale Conosco</h2>
-                            <Form>
-                                <Form.Group controlId="formBasicName">
-                                    <Form.Label>Nome: </Form.Label>
-                                    <Form.Control type="text" placeholder="Nome" />
-                                </Form.Group>
-                                <div>
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Email: </Form.Label>
-                                        <Form.Control type="email" placeholder="Email" />
-                                    </Form.Group>
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Telefone: </Form.Label>
-                                        <Form.Control type="fone" placeholder="(xx) xxxx-xxxx" />
-                                    </Form.Group>
+
+                            <form>
+                                <input type="text" name="name" className="inputName" placeholder="NOME" />
+
+                                <div className="inputDown">
+                                    <div>
+                                        <input type="email" name="email" className="inputContact" placeholder="EMAIL" />
+                                    </div>
+
+                                    <div>
+                                        <input type="telephone" name="telefone" className="inputContact" placeholder="TELEFONE" />
+                                    </div>
                                 </div>
-                                <Button variant="primary" type="button">
-                                    Enviar
-                                </Button>
-                            </Form>
-                            {/* <form>
-                                <label>Nome:</label>
-                                <input type="text" name="name" />
-                                <div>
-                                    Email:
-                                    <input type="email" name="email" />
-                                    Telefone:
-                                    <input type="telephone" name="telefone" />
-                                </div>
-                                <input type="submit" value="Enviar" />
-                            </form> */}
+                                <input className="button-form" type="submit" value="Enviar" />
+                            </form>
                         </div>
                     </div>
                 </div>
