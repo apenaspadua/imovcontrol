6c5ea2c7bfcd535f918498540edd3820605dde8d
guilima2304 <gui.lima2304@gmail.com>
2021-02-03 22:41:29 -0300

    servico de email

--- src/sections/contact-us/index.js
+++ src/sections/contact-us/index.js
@@ -1,36 +1,49 @@
 import React from "react";
-import "./styles.css"
+import "./styles.css";
+import emailjs from "emailjs-com";
 import Form from 'react-bootstrap/Form'
-// import Button from 'react-bootstrap/Button'
-import Button from 'react-bootstrap/Button'
 
 
-export default function ContactUs({id}) {
-        return (
-            <div className={"section-contact-us"}>
-                <div className={"section-opacity-perfil"} id={id}>
+export default function ContactUs({ id }) {
 
-                    <div className="section-content">
-                        <div className="content-contact-us">
-                            <h2>Fale Conosco</h2>
+    function sendEmail(e) {
+        e.preventDefault();
 
-                            <form>
-                                <input type="text" name="name" className="inputName" placeholder="NOME" />
+        emailjs.sendForm('service_wmsii3x', 'template_o8i32dp', e.target, 'user_IKDGgB1T5N5Q74Ywm5OCv')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
 
-                                <div className="inputDown">
-                                    <div>
-                                        <input type="email" name="email" className="inputContact" placeholder="EMAIL" />
-                                    </div>
+            e.target.reset()
+    }
 
-                                    <div>
-                                        <input type="telephone" name="telefone" className="inputContact" placeholder="TELEFONE" />
-                                    </div>
+    return (
+        <div className={"section-contact-us"}>
+            <div className={"section-opacity-perfil"} id={id}>
+
+                <div className="section-content">
+                    <div className="content-contact-us">
+                        <h2>Fale Conosco</h2>
+
+                        <form onSubmit={sendEmail}>
+                            <input type="text" name="name" className="inputName" placeholder="NOME" />
+
+                            <div className="inputDown">
+                                <div>
+                                    <input type="email" name="email" className="inputContact" placeholder="EMAIL" />
                                 </div>
-                                <input className="button-form" type="submit" value="Enviar" />
-                            </form>
-                        </div>
+
+                                <div>
+                                    <input type="telephone" name="phone" className="inputContact" placeholder="TELEFONE" />
+                                </div>
+                            </div>
+                            <input className="button-form" type="submit" value="Enviar" />
+                        </form>
                     </div>
                 </div>
             </div>
-        )
+        </div>
+    )
 }
